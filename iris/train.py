45841d4df0654ccf78b02eab4ae53781fb9f6cc4
JaeHoBahng <jaeho127@gmail.com>
2024-09-23 19:01:24 -0400

    change docstrings

--- iris/train.py
+++ iris/train.py
@@ -8,21 +8,20 @@ class Model:
 
     """
     Class that contains a KNN model to classify iris dataset from sklearn.
-
-    Methods:
-        __init__ - initializes the model by training it on the iris dataset.
-        __call__ - takes a list of data records and returns the predicted labels.
-        _init_data - loads the iris dataset, defines the features and labels, and splits the data into training and evaluation sets.
-        _score - calculates the accuracy score of the model on the evaluation data.
-        _train - trains the KNeighborsClassifier model on the training data.
     """
 
     def __init__(self, test_size=0.5):
+        """
+        initializes the model by training it on the iris dataset.
+        """
         self.dataset = "iris"
         self.architecture = "KNN"
         self._train(test_size)
 
     def __call__(self, data):
+        """
+        takes a list of data records and returns the predicted labels.
+        """
         for record in data:
             if len(record) != len(self.labels) and not all([isinstance(val, float) or isinstance(val, int) for val in record]):
                 raise ValueError(f"Malformed data record {record}")
@@ -30,6 +29,9 @@ class Model:
         yield from (self.labels[label] for label in self.model.predict(data))
 
     def _init_data(self, test_size=0.5):
+        """
+        loads the iris dataset, defines the features and labels, and splits the data into training and evaluation sets.
+        """
         iris_dataset = load_iris()
         self.features = iris_dataset.feature_names
         self.labels = iris_dataset.target_names
@@ -38,10 +40,16 @@ class Model:
         self._eval_data = (x_test, y_test)
 
     def _score(self):
+        """
+        calculates the accuracy score of the model on the evaluation data.
+        """
         preds = self.model.predict(self._eval_data[0])
         return accuracy_score(preds, self._eval_data[1])
 
     def _train(self, test_size=0.5):
+        """
+        trains the KNeighborsClassifier model on the training data.
+        """
         self._init_data()
         classifier = KNeighborsClassifier()
         classifier.fit(*self._train_data)
